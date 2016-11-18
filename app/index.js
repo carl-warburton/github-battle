e088bc655c026f3dbd2f359a67c5745ddd5b65bd
Carl Warburton <CarlWarburton@carls-mbp.ait-sydney.net.au>
2016-11-18 11:59:28 +1100

    user component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,51 @@
+var USER_DATA = {
+  name: 'Carl Warburton',
+  username: 'carl-warburton',
+  image: 'https://avatars3.githubusercontent.com/u/20783849?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function () {
-    console.log(this.props)
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
     return (
-      <div> Hello {this.props.name} </div>
+      <div>
+        <a href={'https://www.github.com./' + this.props.username}>
+        {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld  name="Tyler" anySortOfData={29} />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
