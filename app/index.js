05a0d3d521531da2de739eac873123a4788b78f8
Carl Warburton <CarlWarburton@carls-mbp.ait-sydney.net.au>
2016-11-18 11:28:21 +1100

    setup react app

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    console.log(this.props)
+    return (
+      <div> Hello {this.props.name} </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld  name="Tyler" anySortOfData={29} />,
+  document.getElementById('app')
+);
